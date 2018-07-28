caffbaa8a2a3424aaf5eb92dea4900318af599e8
Winduni <landpool@mail.ru>
2018-07-28 21:01:24 +0700

    second unit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let chooseBtn = document.getElementById('choose'),
+	receiveBtn = document.getElementById('receive'),
+	heading = document.getElementsByTagName('h2')[0],
+	nameInput = document.getElementsByClassName('contactform_name')[0],
+	phoneInput = document.querySelector('contactform_phone'),
+	mailInput = document.querySelectorAll('.contactform_mail')[0],
+	modal = document.querySelector('.modal'),
+	close = document.querySelector('.close'),
+	text = document.getElementsByName('message')[0];
+
+function hover () {
+	heading.textContent = 'Действительно все!'
+};
+
+
+heading.addEventListener('click', hover)
+heading.addEventListener('dblclick', function () {
+	heading.textContent = 'Все включено!'
+});
+
+
+receiveBtn.addEventListener('click', function() {
+	modal.style.display = 'block'
+});
+
+close.addEventListener('click', function() {
+	modal.style.display = 'none'
+});
+
+nameInput.addEventListener('input', function() {
+	text.value = "Меня зовут " + nameInput.value + " и я хочу спросить:";
+	if (nameInput.value == ""){
+		text.value = ''
+	}
+});
